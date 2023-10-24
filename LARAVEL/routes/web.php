7f6853b41766e36ae15a39a79a5be1d4dad82f7f
ADNANLH <adnanharrak5@gmail.com>
2023-10-24 21:38:55 +0100

    24-10

--- LARAVEL/routes/web.php
+++ LARAVEL/routes/web.php
@@ -18,66 +18,105 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 // Route::get('/blog', function () {
 //     return "bonjour";
 // });
 
 
-Route::get('/blog', function (Request $request) {
-    //obtenir le path de cette lien // "blog"
-    // return [
-    //     "name" => $request->path(),
-    //     "article" => "article 1"
-    // ];
-
-    // obtenir le lien copmlet  "http:\/\/127.0.0.1:8000\/blog"
-    // return [
-    //     "name" => $request->url(),
-    //     "article" => "article 1"
-    // ];
-
-    //pour obtenir tout les parametre de url --{"name":"adnan"} -- le lien http://127.0.0.1:8000/blog?name=adnan
-    // return [
-    //     "name" => $request->all(),
-    //     "article" => "article 1"
-    // ];
 
 
-    //pour obtenir une seule parametre de url --"name":"adnan"  -- le lien http://127.0.0.1:8000/blog?name=adnan&age=22
-    return [
-        "name" => $request->input('name', 'lharrak'),  
-        "age" => $request->input('age'),  
-        "article" => "article 1"
-    ];
+
+// Route::get('/blog', function (Request $request) {
+//     //obtenir le path de cette lien // "blog"
+//     // return [
+//     //     "name" => $request->path(),
+//     //     "article" => "article 1"
+//     // ];
+
+//     // obtenir le lien copmlet  "http:\/\/127.0.0.1:8000\/blog"
+//     // return [
+//     //     "name" => $request->url(),
+//     //     "article" => "article 1"
+//     // ];
+
+//     //pour obtenir tout les parametre de url --{"name":"adnan"} -- le lien http://127.0.0.1:8000/blog?name=adnan
+//     // return [
+//     //     "name" => $request->all(),
+//     //     "article" => "article 1"
+//     // ];
 
 
-});
+//     //pour obtenir une seule parametre de url --"name":"adnan"  -- le lien http://127.0.0.1:8000/blog?name=adnan&age=22
+//     return [
+//         "name" => $request->input('name', 'lharrak'),  
+//         "age" => $request->input('age'),  
+//         "article" => "article 1"
+//     ];
+
+
+// });
+
+
+
+
+
+// Route::get('/blog', function (Request $request) {
+//     return [
+//         "link" => \route('blog.show', ['slug' => 'article', 'id' => 13]),
+//         // display =  {"link":"http:\/\/127.0.0.1:8000\/blog\/article-13"}
+      
+//     ];
+
+
+// })->name('blog.index');
+
+
+
+
 // url = http://127.0.0.1:8000/blog/mon-article-1
 //result == ' {"slug":"mon-article","id":"1"} '
 
 //url http://127.0.0.1:8000/blog/mon-article-1?name=adnan
 // result == {"slug":"mon-article","id":"1","name":"adnan"}
 
-Route::get('/blog', function (Request $request) {
-    return [
-        "name" => $request->input('name', 'lharrak'),  
-        "age" => $request->input('age'),  
-        "article" => "article 1"
-    ];
 
+
+
+// Route::get('/blog/{slug}/{id}', function(string $slug, string $id, Request $request){
+//     return [
+//         'slug' => $slug,
+//         'id' => $id,
+//         "name" => $request->input('name', 'lharrak'),  
+
+//     ];
+// })->where([
+//     'id' => '[0-9]+',
+//     'slug' => '[a-z0-9\-]+'
+// ])->name('blog.show');
+
+Route::prefix('/blog')->name('blog.')->group(function (){
+    Route::get('/', function (Request $request) {
+        return [
+            "link" => \route('blog.show', ['slug' => 'article', 'id' => 13]),
+            // display =  {"link":"http:\/\/127.0.0.1:8000\/blog\/article-13"}
+        
+        ];
+
+
+    })->name('index');
+
+    Route::get('/{slug}/{id}', function(string $slug, string $id, Request $request){
+        return [
+            'slug' => $slug,
+            'id' => $id,
+            "name" => $request->input('name', 'lharrak'),  
+
+        ];
+    })->where([
+        'id' => '[0-9]+',
+        'slug' => '[a-z0-9\-]+'
+    ])->name('show');
 
 });
-
-
-
-Route::get('/blog/{slug}-{id}', function(string $slug, string $id, Request $request){
-    return [
-        'slug' => $slug,
-        'id' => $id,
-        "name" => $request->input('name', 'lharrak'),  
-
-    ];
-})->where([
-    'id' => '[0-9]+',
-    'slug' => '[a-z0-9\-]+'
-]);
